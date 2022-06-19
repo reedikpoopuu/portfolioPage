ae6a53cce5f4cb5e2b8922fc6f3448c31cd9b9b4
Reedik Poopuu <reedik.poopuu@gmail.com>
2022-06-19 13:42:55 +0300

    Add some fogotten changes

--- script.js
+++ script.js
@@ -17,18 +17,36 @@ let myfunc = setInterval(function() {
     timer.innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!"
     }, 1000);
 
-    /*let green = document.getElementById("about-me-pic");
-let switcher = document.getElementById("about-me-text");
+// Select the button
+const btn = document.getElementById("about-me-pic");
+// Check for dark mode preference at the OS level
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
+// Get the user's theme preference from local storage, if it's available
+const currentTheme = localStorage.getItem("theme");
+// If the user's preference in localStorage is dark...
+if (currentTheme == "dark") {
+  // ...let's toggle the .dark-theme class on the body
+  document.body.classList.toggle("dark-mode");
+// Otherwise, if the user's preference in localStorage is light...
+} else if (currentTheme == "light") {
+  // ...let's toggle the .light-theme class on the body
+  document.body.classList.toggle("light-mode");
+}
 
-function pictureSwitch() {
-    if(green.style.border == "1px dashed white") {
-        green.style.border = "1px solid white";
-        green.src = "pattern.jpg";
-    } else {
-        green.style.border = "1px dashed white";
-        green.src = "logo.jpg";
-    };
-};
-
-green.addEventListener("click", pictureSwitch); */
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // If the user's OS setting is dark and matches our .dark-mode class...
+  if (prefersDarkScheme.matches) {
+    // ...then toggle the light mode class
+    document.body.classList.toggle("light-mode");
+    // ...but use .dark-mode if the .light-mode class is already on the body,
+    var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
+  } else {
+    // Otherwise, let's do the same thing, but for .dark-mode
+    document.body.classList.toggle("dark-mode");
+    var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+  }
+  // Finally, let's save the current preference to localStorage to keep using it
+  localStorage.setItem("theme", theme);
+});
