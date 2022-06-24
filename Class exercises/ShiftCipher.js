48d61de95639bf6dcb01704153536abc7aa3dafb
Reedik Poopuu <reedik.poopuu@gmail.com>
2022-06-24 23:22:48 +0300

    Add Shift Sipher HTML

--- Class exercises/ShiftCipher.js
+++ Class exercises/ShiftCipher.js
@@ -1,3 +1,38 @@
+document.getElementById("encrypt-button").addEventListener('click', function() {
+  let word = document.getElementById("encrypt-input").value.toLowerCase();
+  let shift = Number(document.getElementById("encrypt-shift").value);
+  let result = '';
+  for(let i = 0; i < word.length; i++) {
+    let j = word.charCodeAt(i);
+    if(j < 97 || j > 122) {
+      result += word[i];
+    } else if(j + shift > 122) {
+      result += String.fromCharCode(word.charCodeAt(i) -26 + shift);
+    } else {
+      result += String.fromCharCode(word.charCodeAt(i) + shift);
+    }
+  }
+  document.getElementById("encrypt-result").innerHTML = result.toUpperCase(); 
+});
+
+document.getElementById("decrypt-button").addEventListener('click', function() {
+  let word = document.getElementById("decrypt-input").value.toLowerCase();
+  let shift = Number(document.getElementById("decrypt-shift").value);
+  let result1 = '';
+  for(let i = 0; i < word.length; i++) {
+    let j = word.charCodeAt(i);
+    if(j < 97 || j > 122) {
+      result1 += word[i];
+    } else if(j - shift < 97) {
+    result1 += String.fromCharCode(word.charCodeAt(i) +26 - shift);
+    } else {
+    result1 += String.fromCharCode(word.charCodeAt(i) - shift);
+    }
+  }
+  document.getElementById("decrypt-result").innerHTML = result1.toLowerCase(); 
+});
+
+/*
 class ShiftCipher {
     constructor(shift) {
       this.shift = shift;
@@ -36,11 +71,4 @@ class ShiftCipher {
       console.log(result);
       return result
     }
-  };
-
-  const cipher = new ShiftCipher(2);
-cipher.encrypt('I love to code!');
-cipher.decrypt('K <3 OA RWRRA'); 
-
-const cipher2 = new ShiftCipher(25);
-cipher2.encrypt('abcdefg');
+  }; */
