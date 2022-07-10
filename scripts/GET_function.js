2b6abc5c0f8d7e1768690627c34e941cc393cfdd
Reedik Poopuu <reedik.poopuu@gmail.com>
2022-07-10 16:44:59 +0300

    Update GET to include an example of async...await version

--- scripts/GET_function.js
+++ scripts/GET_function.js
@@ -50,3 +50,19 @@ const renderResponse = (res) => {
     responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
     return
   }
+
+  //Async...await version
+
+  const getSuggestions = async () => {
+    const wordQuery = inputField.value;
+    const endpoint = url + queryParams + wordQuery;
+    try {
+      const response = await fetch(endpoint, {cache: 'no-cache'});
+      if(response.ok) {
+        const jsonResponse = await response.json();
+        renderResponse(jsonResponse);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
